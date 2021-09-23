99f8e71120a3d10b28fa1075410b60ea3231133a
21prafull <21prafull@gmail.com>
2021-09-23 16:48:37 +0530

    test

---
+++ Prafull/Assignment_2/Que_5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Que_5 {
+	Scanner scan = new Scanner(System.in);
+    
+	int[] array = new int[20];  
+
+    
+    for (int i = 0; i<20; i++) {
+    	System.out.println("Enter Number: ");
+    	int num1 = scan.nextInt();	
+    }
+    
+    int count = 0;
+    while (num1<=num2) {
+    	count = count + num1;
+    	num1++;
+    }
+    System.out.println("Digit sum is: " + count);
+
+}
