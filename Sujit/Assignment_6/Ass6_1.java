478a95b1a49a9d8fceb7356106fbf212cdeb2cd5
Sujit648 <90507160+Sujit648@users.noreply.github.com>
2021-09-29 19:50:48 +0530

    Assignment_6

---
+++ Sujit/Assignment_6/Ass6_1.java
@@ -0,0 +1,13 @@
+
+public class Ass6_1 {
+
+	public static void main(String[] args) {
+		String str = "Sunbem";
+		System.out.println("String : "+str);
+		int index = str.indexOf('b');
+		System.out.printf("b is at index : %d", index);
+		
+
+	}
+
+}
