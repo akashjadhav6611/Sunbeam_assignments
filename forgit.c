5cdd0f72b47cdd48683df8325b766945f582ac77
pranavshelke11 <91021584+pranavshelke11@users.noreply.github.com>
2021-09-29 23:35:44 +0530

    Add files via upload

---
+++ forgit.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+int add, num1, num2;
+printf("HEllo Pranav");
+
+printf("\nEnter first number to perform addition:");
+scanf("%d", &num1);
+
+printf("\nEnter second number to perform addition:");
+scanf("%d", &num2);
+add = num1 + num2;
+
+printf("\naddition of two numbers is :%d",add);
+
+return 0;
+}
