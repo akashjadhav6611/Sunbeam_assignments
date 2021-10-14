76f60a970366f2d56b921584681e918a19d3a451
geronimo-69 <skmahajan4998@gmail.com>
2021-10-14 22:02:45 +0530

    Update PL_SQL_4.sql

--- Saurabh_Mahajan/DBT/Assignments/Assignment_10/PL_SQL_4.sql
+++ Saurabh_Mahajan/DBT/Assignments/Assignment_10/PL_SQL_4.sql
@@ -9,11 +9,11 @@ BEGIN
     DECLARE inch INT;
     DECLARE temp INT;
 
-	SET yard = I / 36;
-    SET temp = I % 36;
-    SET foot = temp / 12;
-    SET inch = temp % 12;
-	SELECT I AS INCHES_GIVEN, yard AS YARD, foot AS FOOT, inch INCH;
+	SET yard = FLOOR(I / 36);
+    SET temp = FLOOR(I % 36);
+    SET foot = FLOOR(temp / 12);
+    SET inch = FLOOR(temp % 12);
+	SELECT I AS INCHES_GIVEN, yard AS YARD, foot AS FOOT, inch AS INCH;
 END;
 $$
 
