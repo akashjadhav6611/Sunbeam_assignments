ddc5fbba802695c943a29781a263539e82f9ff01
geronimo-69 <skmahajan4998@gmail.com>
2021-10-17 08:16:02 +0530

    Create Average.java

---
+++ Saurabh_Mahajan/oopswithjava/Assignment_1/Average.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+
+        double num1;
+        double num2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number");
+        if(sc.hasNextDouble())
+        {
+            num1 = sc.nextDouble();
+            System.out.println("Enter second number");
+            if(sc.hasNextDouble())
+            {
+                num2 = sc.nextDouble();
+                System.out.println("Average : "+(num1+num2)/2);
+            }
+            else
+            {
+                System.out.println(sc.nextDouble()+" is not a double number");
+            }
+        }
+        else
+        {
+            System.out.println(sc.nextDouble()+" is not a double number");
+        }
+        
+    }
+
+}
