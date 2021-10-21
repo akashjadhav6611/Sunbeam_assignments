cdcb6fe72c441d6203da32361d409f0a90a8d4b4
D5_56359_Mustakim_Shaikh <mustakimshaikh455@gmail.com>
2021-10-21 21:03:27 +0530

    DAY5_OOPJAVA_DONE

---
+++ Mustakim_Shaikh/COREJAVA/DAY5_Assign/Fruit.java
@@ -0,0 +1,53 @@
+package com.app.fruits;
+
+public class Fruit {
+
+	// data declaration......
+	private String color;
+	private double weight;
+	private String name;
+	private boolean isFresh;
+	
+	//Parameterized constructor for initialization........ 
+	Fruit(String color,double weight, String name){
+		
+		this.color = color;
+		this.weight = weight;
+		this.name = name;
+		this.isFresh = true;
+	}
+	
+	
+	//boolean method to return state....
+	public boolean isFresh() {
+		
+		return isFresh;
+	}
+	
+	
+	//setter to set fruit is stale or fresh.....
+	public void setFresh(boolean isFresh) {
+		
+		this.isFresh = isFresh;
+	}
+	
+	//getter to return name of fruits:
+	public String getName() {
+		
+		return name;
+	}
+	
+	//taste() method to return taste of fruits override.....
+	public String taste() {
+		
+		return "tell taste";
+	}
+	
+	// toString to display details....
+	public String toString() {
+		
+		return "color = " + color + ", weight = " + weight + " name = " +name ;
+	}
+	
+	
+}
