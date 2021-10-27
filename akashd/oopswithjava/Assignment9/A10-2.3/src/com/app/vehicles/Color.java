ffd21298915d8fd3363a72c1a3cde17fead21495
Akash Deshpande <deshpandeakash47@gmail.com>
2021-10-27 19:50:40 +0530

    akashd : oopswithjava : A10

---
+++ akashd/oopswithjava/Assignment9/A10-2.3/src/com/app/vehicles/Color.java
@@ -0,0 +1,5 @@
+package com.app.vehicles;
+
+public enum Color {
+	RED, GREEN, SILVER, BLUE, ORANGE, BROWN, YELLOW, WHITE, BLACK;
+}
---
+++ akashd/oopswithjava/Assignment9/A10-2.4/src/com/app/vehicles/Vehicle.java
@@ -0,0 +1,57 @@
+package com.app.vehicles;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/*
+ * Vehicle's  state(attributes) --chasisNo : String , color : String ,
+ * category:string, price : double , manufactureDate(Date)
+ */
+public class Vehicle {
+	private String chasisNo;
+	private Color color;
+	private double price;
+	private Date manufactureDate;
+	private Category category;
+
+	// SDF : for parsing + formatting
+	public static SimpleDateFormat sdf;
+	static {
+		sdf = new SimpleDateFormat("yyyy-MM-dd");
+	}
+
+	public Vehicle(String chasisNo, Color color, double price, Date manufactureDate, Category category) {
+		super();
+		this.chasisNo = chasisNo;
+		this.color = color;
+		this.price = price;
+		this.manufactureDate = manufactureDate;
+		this.category = category;
+	}
+	//add overloaded constr to init chasis no of a vehicle
+	public Vehicle(String chasisNo) {
+		this.chasisNo=chasisNo;
+	}
+	
+	public Vehicle(String chasisNo, Color color) {
+		this.chasisNo=chasisNo;
+		this.color = color;
+	}
+
+	@Override
+	public String toString() {
+		return "Vehicle [chasisNo=" + chasisNo + ", color=" + color + ", price=" + price + " manufactured on "
+				+ sdf.format(manufactureDate) + " category " + category;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		System.out.println("in vehicle's equals method");
+		if (o instanceof Vehicle)
+			return (this.chasisNo.equals(((Vehicle) o).chasisNo) && this.color.name().equals(((Vehicle)o).color.name())) ;
+		// String's equals method for : checking content
+																// equality
+		return false;// => incompatible types !!!!!
+	}
+
+}
