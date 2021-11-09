2cde5db2f721c5f5e402b84ada988b1960976d42
geronimo-69 <skmahajan4998@gmail.com>
2021-11-09 17:26:19 +0530

    Java Assignment day18

---
+++ Saurabh_Mahajan/oopswithjava/Assignment_day18/src/tester/TestStudent.java
@@ -0,0 +1,168 @@
+package tester;
+
+import static utils.CollectionUtils.*;
+import static java.util.Comparator.comparing;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+import com.app.core.Student;
+import com.app.core.Subject;
+
+public class TestStudent 
+{
+
+	public static void main(String[] args) 
+	{
+		List<Student> studentList = sampleData();
+		Map<String,Student> studentMap = sampleMapData(studentList);
+		
+		boolean key = false;
+		try(Scanner scan = new Scanner(System.in))
+		{
+			while(!key)
+			{
+				try
+				{
+					System.out.println("\n1.Display by list 2.Sort by gpa 3.Sum of gpa by specific subject "
+							+ "\n4.Average of gpa by specific subject 5.Find topper of subject \n6.Find failed students "
+							+ "7.Find no of students with distinction \n8.Display by map 9.Sort by roll no(map) "
+							+ "10.Sort by dob with map");
+					System.out.println("0.Exit");
+					System.out.println("Enter choice : ");
+					
+					switch(scan.nextInt())
+					{
+					case 1:
+						//by lambda
+						studentList.stream().
+						forEach(s -> System.out.println(s));
+						//by method reference
+						//studentList.forEach(System.out::println);
+						break;
+						
+					case 2:
+						//3 methods to do - all work
+						//Comparator<Student> gpaComp2 = (s1,s2) -> ((Double)s1.getGpa()).compareTo(s2.getGpa());
+						//Comparator<Student> gpaComp3 = comparing(s -> s.getGpa());
+						Comparator<Student> gpaComp = comparing(Student :: getGpa);
+						studentList.sort(gpaComp);
+						System.out.println("List sorted by gpa : ");
+						studentList.forEach(System.out::println);
+						break;
+						
+					case 3:
+						System.out.println("Enter subject name : ");
+						Subject sub3 = Subject.valueOf(scan.next().toUpperCase());//string to enum
+						double sum =studentList.stream().//converting list to stream
+									filter(s -> s.getSubject() == sub3).//intermediate operation - filtering by subject
+									mapToDouble(Student :: getGpa).//intermediate - getting(mapping) gpa's of filtered subjects
+									sum();//addition - higher order function
+						
+						System.out.println("Sum : "+sum);
+						break;
+						
+					case 4:
+						System.out.println("Enter subject name : ");
+						Subject sub4 = Subject.valueOf(scan.next().toUpperCase());//string to enum
+						double avg =studentList.stream().//converting list to stream
+									filter(s -> s.getSubject() == sub4).//intermediate operation - filtering by subject
+									mapToDouble(Student :: getGpa).//intermediate - getting(mapping) gpa's of filtered subjects
+									average().//average - higher order function
+									orElse(0);//can be tried with orElseThrow(supplier)
+						
+						System.out.println("Average : "+avg);
+						break;
+						
+					case 5:
+						System.out.println("Enter subject name : ");
+						Subject sub5 = Subject.valueOf(scan.next().toUpperCase());//string to enum
+						double max = studentList.stream().//converting list to stream
+									filter(s -> s.getSubject() == sub5).//intermediate operation - filtering by subject
+									mapToDouble(Student :: getGpa).//intermediate - getting(mapping) gpa's of filtered subjects
+									max().//maximum - higher order function
+									getAsDouble();
+						
+						studentList.stream().//converting list to stream
+						filter(s -> s.getGpa() == max).//intermediate - filtering max gpa in given subject
+						forEach(s -> System.out.println("Student name : "+s.getName()));//printing name of topper - terminating operation
+						break;
+						
+					case 6:
+						System.out.println("Enter subject name : ");
+						Subject sub6 = Subject.valueOf(scan.next().toUpperCase());//string to enum
+						
+						System.out.println("Failed student names : ");
+						studentList.stream().//converting list to stream
+						filter(s -> s.getSubject() == sub6).//intermediate operation - filtering by subject
+						filter(s -> s.getGpa() < 5).//intermediate - getting(mapping) gpa's less tha 5 of filtered subjects
+						forEach(s -> System.out.println(s.getName()));//printing - higher order function
+						//can be done with map(function) before forEach
+						break;
+						
+					case 7:
+						System.out.println("Enter subject name : ");
+						Subject sub7 = Subject.valueOf(scan.next().toUpperCase());//string to enum
+						
+						System.out.println("Student names with distinction : ");
+						long count = studentList.stream().//converting list to stream
+									filter(s -> s.getSubject() == sub7).//intermediate operation - filtering by subject
+									filter(s -> s.getGpa() > 7.5).//intermediate - getting(mapping) gpa's greater than 7.5 of filtered subjects
+									count();//returns long value
+						
+						//printing number of students stored in count
+						System.out.println("No of students with distinction in "+sub7+" : "+count);
+						break;
+						
+					case 8:
+						//higher order function
+						studentMap.forEach((s,v) -> System.out.println(v));
+						break;
+						
+					case 9:
+						//this constructor of treemap automatically sorts by key i.e. String rollNo
+						TreeMap<String, Student> studentTreeMap = new TreeMap<>(studentMap);
+						//printing sorted map values
+						studentTreeMap.forEach((s,v) -> System.out.println(v));
+						break;
+						
+					case 10:
+						//converting map to collection by values() method
+						Collection<Student> studentCollection = studentMap.values();
+						//creating a custom sorting comparator
+						Comparator<Student> dobComp = comparing(Student :: getDob);
+						
+						studentCollection.stream().//Collection to stream
+						sorted(dobComp).//sorting by custom comparator
+						forEach(System.out::println);//termination function
+						break;
+						
+					case 0:
+						System.out.println("Exited the program...");
+						key = true;
+						break;
+						
+					default:
+						System.out.println("Invalid choice !!!");
+						break;
+					}
+				}
+				catch(Exception e)
+				{
+					System.out.println(e);
+				}
+				scan.nextLine();
+			}
+			
+		}catch(Exception e)
+		{
+			System.out.println(e);
+		}
+
+	}
+
+}
